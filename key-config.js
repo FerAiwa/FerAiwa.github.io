aabd7e5c45f53ec3be585551c4e2057c6efd6718
Fer Ayguavives <47696430+FerAiwa@users.noreply.github.com>
2019-02-27 00:09:04 +0100

    Delete key-config.js

--- key-config.js
+++
@@ -1,8 +0,0 @@
-function setBtnListeners () {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(btn => btn.addEventListener('click', reactKey))
-}
-
-function setKeyListeners () {
-    document.addEventListener('keydown', reactKey)
-}
