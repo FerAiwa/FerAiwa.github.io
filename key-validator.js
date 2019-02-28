b1d605dc78c400f2d771ea446bc8db060ac24c6a
Fer Ayguavives <47696430+FerAiwa@users.noreply.github.com>
2019-02-28 01:09:10 +0100

    Update key-validator.js

--- key-validator.js
+++ key-validator.js
@@ -1,5 +1,4 @@
 function validateInput (str) {
-    console.log('validating input', str)
     const validation = str.trim().split("")
         .map(isValidChar)
         .reduce(x => x === true)
@@ -11,6 +10,5 @@ function isValidChar (char) {
     const OPERATORS = ['+','-','*','/'];
     const validation = /[0-9,.]/.test(char) || OPERATORS.includes(char); 
     if(!validation) throw 'Invalid character. Try again.';
-    console.log(char, validation)
     return validation
 }
